bc448b9c49c4d530a2310b4beb5ceb70c1d8262e
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-11-09 10:19:07 +0100

    Fix eslint errors

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,21 +1,17 @@
-import React from "react"
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Provider } from "react-redux";
-import Greeting from "./Greeting";
-import store from "../redux/configureStore";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Greeting from './Greeting';
+import store from '../redux/configureStore';
 
-class App extends React.Component {
-  render () {
-    return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Greeting />} />
-          </Routes>
-        </BrowserRouter>
-      </Provider>
-    );
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
 
-export default App
+export default App;
--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,18 +1,21 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getGreetingsSuccess } from "../redux/greetings/greetings";
+import { getGreetingsSuccess } from '../redux/greetings/greetings';
 
 const Greeting = () => {
   const dispatch = useDispatch();
   const greeting = useSelector((state) => state.greetingsReducer.message);
-  
+
   useEffect(() => {
     dispatch(getGreetingsSuccess());
   }, [dispatch]);
 
   return (
     <>
-      <h1><i>{greeting}</i>!!</h1>
+      <h1>
+        <i>{greeting}</i>
+        !!
+      </h1>
     </>
   );
 };
